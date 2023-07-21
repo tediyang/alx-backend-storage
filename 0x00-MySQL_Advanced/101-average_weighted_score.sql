eec22c877ef6bf4e7a63795b6b3a33186ed1522e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-21 22:56:46 +0100

    averaged weighted score for all students

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,21 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUsers that computes and store the average
+-- weighted score for all students.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+  WITH t1 AS (
+    SELECT SUM(p.weight * c.score) / SUM(p.weight) w, c.user_id FROM projects p
+    JOIN corrections c
+    ON c.project_id = p.id
+    GROUP BY 2
+  )
+
+  UPDATE users
+    SET average_score = (
+      SELECT w FROM t1
+      WHERE t1.user_id = users.id
+    );
+END //
+DELIMITER ;
