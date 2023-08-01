6bf950a4f0173f41a22a8532bd50fb4faf48a511
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-01 01:13:47 +0100

    exercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Setting up a class that writes to redis """
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """
+        A class Cache that writes and stores value in Redis
+    """
+    def __init__(self):
+        """
+            setup the redis database
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | float | bytes | int) -> str:
+        """_summary_
+
+        Args:
+            data (str | float | bytes | int): the value to save
+
+        Returns:
+            str: the key returned.
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
